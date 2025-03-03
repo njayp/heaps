be6513f03adf8841385663c9ac8955200ed9d9a0
nick powell <nickjaypowell@gmail.com>
2025-03-02 17:36:45 -0800

    go 1.22 for compat

--- heap.go
+++ heap.go
@@ -8,8 +8,8 @@ import (
 // `less` determines the heap type:
 // - For a min-heap, use `less = func(a, b T) bool { return a < b }`.
 // - For a max-heap, use `less = func(a, b T) bool { return a > b }`.
-func NewHeap[T any](less func(a, b T) bool, initialData ...T) *Heap[T] {
-	h := &Heap[T]{
+func NewHeap[T any](less func(a, b T) bool, initialData ...T) Heap[T] {
+	h := Heap[T]{
 		h: &genericHeapImpl[T]{
 			data: initialData,
 			less: less,
@@ -29,21 +29,21 @@ type Heap[T any] struct {
 }
 
 // Push pushes the element x onto the heap. The complexity is O(log n) where n = h.Len().
-func (h *Heap[T]) Push(t T) {
+func (h Heap[T]) Push(t T) {
 	heap.Push(h.h, t)
 }
 
 // Pop removes and returns the minimum element (according to Less) from the heap. The complexity is O(log n) where n = h.Len(). Panics if the heap is empty.
-func (h *Heap[T]) Pop() T {
+func (h Heap[T]) Pop() T {
 	return heap.Pop(h.h).(T)
 }
 
 // Peek returns the minimum element (according to Less) from the heap without removing it. The complexity is O(1). Panics if the heap is empty.
-func (h *Heap[T]) Peek() T {
+func (h Heap[T]) Peek() T {
 	return h.h.data[0]
 }
 
 // Size returns the number of elements in the heap.
-func (h *Heap[T]) Size() int {
+func (h Heap[T]) Size() int {
 	return h.h.Len()
 }
--- presets.go
+++ presets.go
@@ -1,9 +1,9 @@
 package heaps
 
-func NewMinHeap(initialData ...int) *Heap[int] {
+func NewMinHeap(initialData ...int) Heap[int] {
 	return NewHeap(func(a, b int) bool { return a < b }, initialData...)
 }
 
-func NewMaxHeap(initialData ...int) *Heap[int] {
+func NewMaxHeap(initialData ...int) Heap[int] {
 	return NewHeap(func(a, b int) bool { return a > b }, initialData...)
 }
